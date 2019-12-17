d3d08698cc764ac66ebcea878f5ca5ba3b6673d1
vaseQ-NotAllow <hayvasahay@gmail.com>
2019-12-17 07:51:57 +0500

    test

--- src/test/java/BotCoreTest.java
+++ src/test/java/BotCoreTest.java
@@ -1,75 +1,55 @@
-//import Bot.BotCore;
-//import Generator.TaskGenerator;
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class BotCoreTest {
-//    @Test
-//    public void parseUserInput_ShouldReturnWarningAboutNameInStartOfMessage_IfGetMessageWithoutName(){
-//        BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
-//        String input = "asd";
-//        String excepted = "Message should start with @username";
-//
-//        String actual = botCore.parseUserInput(input);
-//
-//        Assert.assertEquals(excepted, actual);
-//    }
-//
-//    @Test
-//    public void parseUserInput_ShouldReturnWarningAboutCommand_IfGetMessageOnlyWithUsername(){
-//        BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
-//        String input = "@Test";
-//        String excepted = "Message must have next form \"@username input(Command)\"";
-//
-//        String actual = botCore.parseUserInput(input);
-//
-//        Assert.assertEquals(excepted, actual);
-//    }
-//
-//    @Test
-//    public void parseUserInput_ShouldReturnTaskCondition_IfGetCommandTask(){
-//        BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
-//        String input = "@Test /task";
-//        String excepted = "What the sum of 5+6?";
-//
-//        String actual = botCore.parseUserInput(input);
-//
-//        Assert.assertEquals(excepted, actual);
-//    }
-//
-//    @Test
-//    public void parseUserInput_ShouldReturnInfo_IfGetFirstNotCommandMessage(){
-//        BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
-//        String input = "@Test task";
-//        String excepted = "Test";
-//
-//        String actual = botCore.parseUserInput(input);
-//
-//        Assert.assertEquals(excepted, actual);
-//    }
-//
-//    @Test
-//    public void parseUserInput_ShouldReturnInfo_IfGetHelpCommand(){
-//        BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
-//        String input = "@Test /help";
-//        String excepted = "Test";
-//
-//        String actual = botCore.parseUserInput(input);
-//
-//        Assert.assertEquals(excepted, actual);
-//    }
-//
-//    @Test
-//    public void parseUserInput_ShouldReturnMessageAboutGettingNewTask_IfGetMessageAfterTakingAnswer(){
-//        BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
-//        String task = "@Test /task";
-//        String input = "@Test asd";
-//        String excepted = "You need to get task first";
-//
-//        botCore.parseUserInput(task);
-//        botCore.parseUserInput(input);
-//        String actual = botCore.parseUserInput(input);
-//
-//        Assert.assertEquals(excepted, actual);
-//    }
-//}
+import Bot.BotCore;
+import Generator.TaskGenerator;
+import Messager.ChatId;
+import Messager.Message;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BotCoreTest {
+    @Test
+    public void parse_ShouldReturnTaskCondition_IfGetCommandTask(){
+        BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
+        Message input = new Message("/task", new ChatId("test"));
+        Message excepted = new Message("What the sum of 5+6?", new ChatId("test"));
+
+        Message actual = botCore.parse(input);
+
+        Assert.assertEquals(excepted, actual);
+    }
+
+    @Test
+    public void parse_ShouldReturnInfo_IfGetFirstNotCommandMessage(){
+        BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
+        Message input = new Message("task", new ChatId("test"));
+        Message excepted = new Message("Test", new ChatId("test"));
+
+        Message actual = botCore.parse(input);
+
+        Assert.assertEquals(excepted, actual);
+    }
+
+    @Test
+    public void parse_ShouldReturnInfo_IfGetHelpCommand(){
+        BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
+        Message input = new Message("/help", new ChatId("test"));
+        Message excepted = new Message("Test", new ChatId("test"));
+
+        Message actual = botCore.parse(input);
+
+        Assert.assertEquals(excepted, actual);
+    }
+
+    @Test
+    public void parse_ShouldReturnMessageAboutGettingNewTask_IfGetMessageAfterTakingAnswer(){
+        BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
+        Message task = new Message("/task", new ChatId("test"));
+        Message input = new Message("asd", new ChatId("test"));
+        Message excepted = new Message("You need to get task first", new ChatId("test"));
+
+        botCore.parse(task);
+        botCore.parse(input);
+        Message actual = botCore.parse(input);
+
+        Assert.assertEquals(excepted, actual);
+    }
+}
