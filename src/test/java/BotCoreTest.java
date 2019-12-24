180554ce2f239102a567396758b5999dbf330540
vaseQ-NotAllow <hayvasahay@gmail.com>
2019-12-24 09:33:30 +0500

    Tests

--- src/test/java/BotCoreTest.java
+++ src/test/java/BotCoreTest.java
@@ -6,15 +6,19 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class BotCoreTest {
+    private void message_Assert(Message excepted, Message actual){
+        Assert.assertEquals(excepted.getText(), actual.getText());
+        Assert.assertEquals(excepted.getId().getChatId(), actual.getId().getChatId());
+    }
     @Test
-    public void parse_ShouldReturnTaskCondition_IfGetCommandTask(){
+    public void test_Parse_ShouldReturnTaskCondition_IfGetCommandTask(){
         BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
         Message input = new Message("/task", new ChatId("test"));
         Message excepted = new Message("What the sum of 5+6?", new ChatId("test"));
 
         Message actual = botCore.parse(input);
 
-        Assert.assertEquals(excepted, actual);
+        message_Assert(excepted, actual);
     }
 
     @Test
@@ -25,7 +29,7 @@ public class BotCoreTest {
 
         Message actual = botCore.parse(input);
 
-        Assert.assertEquals(excepted, actual);
+        message_Assert(excepted, actual);
     }
 
     @Test
@@ -36,7 +40,7 @@ public class BotCoreTest {
 
         Message actual = botCore.parse(input);
 
-        Assert.assertEquals(excepted, actual);
+        message_Assert(excepted, actual);
     }
 
     @Test
@@ -50,6 +54,21 @@ public class BotCoreTest {
         botCore.parse(input);
         Message actual = botCore.parse(input);
 
-        Assert.assertEquals(excepted, actual);
+        message_Assert(excepted, actual);
+    }
+
+    @Test
+    public void core_ShouldCorrectlyWork_WithSeveralUsers(){
+        BotCore botCore = new BotCore(new TaskGenerator("src/main/resources/Tasks.properties"), "Test");
+        Message firs_user_get_task = new Message("/task", new ChatId("test"));
+        Message second_user_put_answer = new Message("11", new ChatId("test1"));
+        Message excepted_first_get_task = new Message("What the sum of 5+6?", new ChatId("test"));
+        Message excepted_second_get_info = new Message("Test", new ChatId("test1"));
+
+        Message actual1 = botCore.parse(firs_user_get_task);
+        Message actual2 = botCore.parse(second_user_put_answer);
+
+        message_Assert(excepted_first_get_task, actual1);
+        message_Assert(excepted_second_get_info, actual2);
     }
 }
