f8c6b68c62b1a14491d5b0d94cce736edde1a248
vaseQ-NotAllow <hayvasahay@gmail.com>
2019-12-24 09:32:54 +0500

    SQL tasks generator

--- src/main/java/Generator/SQLTaskGenerator.java
+++ src/main/java/Generator/SQLTaskGenerator.java
@@ -9,18 +9,16 @@ public class SQLTaskGenerator implements IGenerator {
     private Integer lastTaskId;
     private ArrayList<Task> taskList;
 
-    public SQLTaskGenerator() throws SQLException, ClassNotFoundException {
+    public SQLTaskGenerator(String userName, String password, String connectionUrl)
+            throws SQLException, ClassNotFoundException {
         taskList = new ArrayList<Task>();
-        String userName = "root";
-        String password = "root";
-        String connectionUrl = "jdbc:mysql://localhost:3306/bottask";
-        Class.forName("com.mysql.jdbc.Driver");
+
         try(Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
             Statement statement = connection.createStatement()) {
             System.out.println("We have SQL connection!");
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM Task");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM test№2");
             while (resultSet.next()) {
-                Task task = new Task(resultSet.getString("condition"), resultSet.getString("answer"));
+                Task task = new Task(resultSet.getString("t_condition"), resultSet.getString("answer"));
                 taskList.add(task);
             }
         }
